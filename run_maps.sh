8c7a26ac9e7cdac8aefa579ce29943ae5a1697c8
Ben <bsmith23@students.claremontmckenna.edu>
2023-04-15 15:19:44 -0700

    cmmit2

--- run_maps.sh
+++ run_maps.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
 
-# set input and output paths
-input_folder="./cs46hw/twitter_coronavirus"
-output_folder="./cs46hw/twitter_coronavirus/outputs"
-
-# loop over each file in the input folder
-for input_path in ${input_folder}/*2020-*.zip; do
-    # set output path for current file
-    output_path="${output_folder}/$(basename ${input_path} .zip)"
-
-    # run map.py command in the background using nohup and &
-    nohup python map.py --input_path "${input_path}" --output_folder "${output_path}" > "${output_path}.log" 2>&1 &
-
-    # print message
-    echo "Started processing ${input_path}. Output will be saved to ${output_path}"
+for input_file in /data/Twitter\ dataset/geoTwitter20-*.zip; do
+  echo "Running map.py on $input_file"
+ 
+  nohup ./src/map.py --input_path=${input_file} &
 done
+
+echo "All map.py commands submitted. Check the output directory for results."
