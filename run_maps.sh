bfdaab09fbb30cf36658c46235a5b730a703d306
Ben <bsmith23@students.claremontmckenna.edu>
2023-04-15 15:57:42 -0700

    test

--- run_maps.sh
+++ run_maps.sh
@@ -5,5 +5,3 @@ for input_file in /data/Twitter\ dataset/geoTwitter20-*.zip; do
  
   nohup ./src/map.py --input_path=${input_file} &
 done
-
-echo "All map.py commands submitted. Check the output directory for results."
